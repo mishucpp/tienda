71ed2bcf7a8a1e1eb6754f08d9571a28309669f0
adrian <pistrisrivera@gmail.com>
2018-12-13 20:24:36 -0600

    se xompuso error variables

--- carrito.php
+++ carrito.php
@@ -7,36 +7,36 @@ if(isset($_POST['btnAccion'])){
         case 'Agregar' :
         if(is_numeric( openssl_decrypt($_POST['id'],COD,KEY))){
                $ID=openssl_decrypt($_POST['id'],COD,KEY);
-               $mensaje.="oK ID correcto". $ID;
+               $mensaje.="oK ID correcto". $ID."<br>";
         }else{
-            $mensaje.="ups ID incorrecto". $ID;
+            $mensaje.="ups ID incorrecto". $ID."<br>";
 
-        }asdjasdkjaksdaisd aápsdoajisd qwdqwdsadasd
-        break;
-        if(is_numeric( openssl_decrypt($_POST['nombre'],COD,KEY))){
+        break;}
+        
+        if(is_string( openssl_decrypt($_POST['nombre'],COD,KEY))){
             $NOMBRE=openssl_decrypt($_POST['nombre'],COD,KEY);
-            $mensaje.="oK nombre correcto". $NOMBRE;
+            $mensaje.="oK nombre correcto". $NOMBRE."<br>";
      }else{
-         $mensaje.="ups nombre incorrecto". $NOMBRE;
+         $mensaje.="ups nombre incorrecto". $NOMBRE."<br>";
 
-     }
-     break;
-     if(is_numeric( openssl_decrypt($_POST['cantidad'],COD,KEY))){
+     break;}
+     
+     if(is_string( openssl_decrypt($_POST['cantidad'],COD,KEY))){
         $CANTIDAD=openssl_decrypt($_POST['cantidad'],COD,KEY);
-        $mensaje.="oK cantidad correcto". $CANTIDAD;
+        $mensaje.="oK cantidad correcto". $CANTIDAD."<br>";
  }else{
-     $mensaje.="ups cantidad incorrecto". $CANTIDAD;
+     $mensaje.="ups cantidad incorrecto". $CANTIDAD."<br>";
 
- }
- break;
+ break;}
+ 
  if(is_numeric( openssl_decrypt($_POST['precio'],COD,KEY))){
     $PRECIO=openssl_decrypt($_POST['precio'],COD,KEY);
-    $mensaje.="oK precio correcto". $PRECIO;
+    $mensaje.="oK precio correcto". $PRECIO."<br>";
 }else{
- $mensaje.="ups precio incorrecto". $PRECIO;
+ $mensaje.="ups precio incorrecto". $PRECIO."<br>";
 
-}
 break;
+}
     }
 }
 
