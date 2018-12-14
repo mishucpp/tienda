b1e79f37844660d58a3dc466d1e9bef0b8f68fdb
adrian <pistrisrivera@gmail.com>
2018-12-13 21:37:56 -0600

    diseño de la tablaCarrito

--- mostrarCarrito.php
+++ mostrarCarrito.php
@@ -6,11 +6,30 @@ include 'template/cabecera.php'
 ?>
 <br>
 <h3>Lista del carrito</h3>
+<table class="table table-light table-bordered">
 <tbody>
     <tr>
-        <td>Contenido del carrito</td>
+      <th width="40%">Descripcion</th>
+      <th width="15%" class="tex-center">Cantidad</th>
+      <th width="20%" class="tex-center">Precio</th>
+      <th width="20%" class="tex-center">Total</th>
+      <th width="5%">--</th>
+    </tr>
+    <tr>
+      <td width="40%"></td>
+      <td width="15%" class="tex-center"></td>
+      <td width="20%" class="tex-center"></td>
+      <td width="20%" class="tex-center"></td>
+      <td width="5%"><button class="btn btn-danger" type="button">Eliminar</button></td>
+    </tr>
+    <tr>
+ <td colspan="3" align="right"><h3>Total</h3></td>
+ <td align="right"><h3><?php echo number_format(300,2); ?></h3></td>
+ <td></td>
     </tr>
 </tbody>
+
+</table>
   
 <?php include 'template/pie.php'
         ?>
